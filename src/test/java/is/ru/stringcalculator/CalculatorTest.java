0d40f1f3e390feac3001e67ce1991bef808b4767
Vikingur Hauksson <vikingur13@i-7f3b5dd9.csd07cloud.internal>
2015-10-13 16:52:26 +0000

    Main class and Test class cleaned

--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -8,25 +8,4 @@ public class CalculatorTest {
 	public static void main(String args[]) {
       org.junit.runner.JUnitCore.main("is.ru.stringcalculator.CalculatorTest");
     }
-
-	@Test
-	public void testEmptyString() {
-		assertEquals(0, Calculator.add(""));
-	}
-
-	@Test
-	public void testOneNumber() {
-		assertEquals(1, Calculator.add("1"));
-	}
-
-	@Test
-	public void testTwoNumbers() {
-		assertEquals(3, Calculator.add("1,2"));
-	}	
-
-	@Test
-    public void testMultipleNumbers(){
-    	assertEquals(6, Calculator.add("1,2,3"));
-    }
-
 }
